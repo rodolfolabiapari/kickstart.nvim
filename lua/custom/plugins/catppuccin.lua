3dec3127ec514bfffcb7f29157eaba9f91adbc1d
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-04-22 18:03:02 -0300

    nvim comments and theme

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,9 +1,9 @@
+-- Add theme to the neovim
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
   config = function()
-    -- vim.cmd.colorscheme("catppuccin-latte")
-    vim.cmd.colorscheme 'catppuccin-frappe'
+    vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
