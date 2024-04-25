ba98f588bcb28135c96227e5e6609e6705aa6f75
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2024-04-25 08:24:40 -0300

    Adicao de image preview e nav toggle

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -6,9 +6,89 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    -- Optional image support in preview window: See `# Preview Mode` for more
+    -- information
+    '3rd/image.nvim',
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = '[C] [N]avigation' })
+    -- Show navavigation bar with C-n
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle=true reveal=true<CR>', { desc = '[N]avigation Bar' })
+
+    require('neo-tree').setup {
+      -- Close Neo-tree if it is the last window left in the tab
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            --"node_modules"
+          },
+          hide_by_pattern = { -- uses glob style patterns
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            --".gitignored",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            '.DS_Store',
+            '.git',
+            --"thumbs.db"
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            --".null-ls_*",
+          },
+        },
+        follow_current_file = {
+          enabled = false, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+        -- in whatever position is specified in window.position
+        -- "open_current",  -- netrw disabled, opening a directory opens within the
+        -- window like netrw would, regardless of window.position
+        -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+        use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+        -- instead of relying on nvim autocmd events.
+        window = {
+          mappings = {
+            ['<bs>'] = 'navigate_up',
+            ['.'] = 'set_root',
+            ['H'] = 'toggle_hidden',
+            ['/'] = 'fuzzy_finder',
+            ['D'] = 'fuzzy_finder_directory',
+            ['#'] = 'fuzzy_sorter', -- fuzzy sorting using the fzy algorithm
+            -- ["D"] = "fuzzy_sorter_directory",
+            ['f'] = 'filter_on_submit',
+            ['<c-x>'] = 'clear_filter',
+            ['[g'] = 'prev_git_modified',
+            [']g'] = 'next_git_modified',
+            ['o'] = { 'show_help', nowait = false, config = { title = 'Order by', prefix_key = 'o' } },
+            ['oc'] = { 'order_by_created', nowait = false },
+            ['od'] = { 'order_by_diagnostics', nowait = false },
+            ['og'] = { 'order_by_git_status', nowait = false },
+            ['om'] = { 'order_by_modified', nowait = false },
+            ['on'] = { 'order_by_name', nowait = false },
+            ['os'] = { 'order_by_size', nowait = false },
+            ['ot'] = { 'order_by_type', nowait = false },
+            -- ['<key>'] = function(state) ... end,
+          },
+          fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+            ['<down>'] = 'move_cursor_down',
+            ['<C-n>'] = 'move_cursor_down',
+            ['<up>'] = 'move_cursor_up',
+            ['<C-p>'] = 'move_cursor_up',
+            -- ['<key>'] = function(state, scroll_padding) ... end,
+          },
+        },
+
+        commands = {}, -- Add a custom command or override a global one using the same function name
+      },
+    }
   end,
 }
