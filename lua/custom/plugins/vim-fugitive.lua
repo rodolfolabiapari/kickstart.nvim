bc13a750e33d4574a98fa952ede9dba663e6262c
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-10-31 08:44:49 -0300

    feat(vim-fugitive): Configura o autocarregamento com lazy = false

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -1,8 +1,12 @@
 -- Use :Git inside nvim
 -- https://github.com/tpope/vim-fugitive
+-- :help fugitive
+-- :G blame
 
 return {
   "tpope/vim-fugitive",
+  -- Carrega automaticamente no Lazy
+  lazy = false,
   keys = {
     -- Abre o :Gstatus (janela de status do Git)
     { "<leader>gs", "<cmd>tabnew | Gstatus<cr>", desc = "Git Status (Fugitive)" }
