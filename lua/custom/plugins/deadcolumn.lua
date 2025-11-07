251f3cc6fad671858db0aad38db62e54da82cf5a
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-11-07 16:25:07 -0300

    chore(deadcolumn): Adicao de comentários a serem utilizados no futuro

--- lua/custom/plugins/deadcolumn.lua
+++ lua/custom/plugins/deadcolumn.lua
@@ -1,2 +1,13 @@
 -- Creates a dynamic 80 line
-return { 'Bekaboo/deadcolumn.nvim' }
+return {
+  'Bekaboo/deadcolumn.nvim',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  -- opts = {
+  --   blending = {
+  --     threshold = 0.75,
+  --   },
+  --   warning = {
+  --     colorcode = '#ED8796',
+  --   }
+  -- }
+}
