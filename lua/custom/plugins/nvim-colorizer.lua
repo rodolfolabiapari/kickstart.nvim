2cc64198a727901b9eb6b5cfd55204ccfdcef793
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-11-08 10:28:54 -0300

    wip(nvim-colorizer): Ver cores nos códigos, mas nao esta funcionando yet

---
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -0,0 +1,11 @@
+-- Exibe a cor quando uma cor é exibida em texto
+-- https://github.com/norcalli/nvim-colorizer.lua
+-- Não esta funcionando TODO verificar
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    opts = {
+      -- options
+    },
+  },
+}
