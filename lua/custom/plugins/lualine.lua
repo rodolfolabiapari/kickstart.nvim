fc6b872233ab02efc79ba9261d250717848f713d
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-11-16 13:25:36 -0300

    feat(lualine): removendo info inuteis

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,10 +1,29 @@
 -- Show a status bar at bottom of neovim screen
+-- https://github.com/nvim-lualine/lualine.nvim
 return {
   'nvim-lualine/lualine.nvim',
   config = function()
     require('lualine').setup {
       options = {
         theme = 'dracula',
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'buffers'},
+        lualine_x = {'lsp_status', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'selectioncount', 'location'}
+
+      },
+      -- nao entendi esse inactive
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
       },
     }
   end,
